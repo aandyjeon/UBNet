abeefd612fde2cf7db045d2a41faa327c6750399
M. Jeon <50168126+aandyjeon@users.noreply.github.com>
2022-05-10 17:50:05 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #train basline
-python main.py -e celeba --is_train --is_valid --cuda --imagenet_pretrain --data CelebA-HQ --n_class 2 --lr 0.0001 --max_step 2 --gpu 1 --model vgg11 --lr_decay_period 10 --lr_decay_rate 0.1
+python base_model/main_base.py -e celebA_train --imagenet_pretrain --data_dir dataset --save_dir exp --data CelebA-HQ --is_train --model vgg11 --batch_size=32 --max_step=20 --lr=0.0001 --cuda --gpu=0 --lr_scheduler step --lr_decay_period=10
 
 #train orthonet
-python main.py -e celeba_orth --is_train --is_valid --orthonet --cuda --use_pretrain True --checkpoint celeba/checkpoint_step_1.pth --data CelebA-HQ --n_class 2 --lr 0.0001 --max_step 2 --lr_decay_rate 0.1 --lr_decay_period 10 --model vgg11 --gpu 1
+python main.py -e celebA_ubnet_train --is_train --ubnet --cuda --checkpoint exp/celebA_train/checkpoint_step_19.pth --data CelebA-HQ --data_dir dataset --save_dir exp --lr=0.0001 --max_step=20 --gpu=0 --batch_size=32 --model vgg11 --lr_scheduler step --lr_decay_period=10
