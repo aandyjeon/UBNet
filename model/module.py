113f2f235b376cb2b18def19dd2f92a27c53f753
M. Jeon <50168126+aandyjeon@users.noreply.github.com>
2022-07-26 22:07:07 +0900

    Update module.py

--- model/module.py
+++ model/module.py
@@ -18,17 +18,13 @@ class Orthorconv2d(nn.Module):
 
     def orthogonal_update(self):
         self.zero_grad()
-#        self.loss = cos_similarity(self.conv.weight.view(self.in_channel*self.out_channel//self.groups, -1))
-#        print(f"[DEBUG] self.conv.weight: {self.conv.weight.shape}")
         self.loss = cos_similarity(self.conv.weight.view(self.groups, -1))
-#        print(f"[DEBUG:module:Orthorconv2d] self.loss: {self.loss}")
         self.loss.backward()
         self.opt_orth.step()
 
     def forward(self, feat):
         if self.training:
             self.orthogonal_update()
-#        self.loss = 0
         return self.conv(feat), self.loss
 
 
@@ -44,7 +40,6 @@ class OrthorTransform(nn.Module):
 
     def orthogonal_update(self):
         self.zero_grad()
-#        self.loss = cos_similarity(self.weight.view(self.c_dim, -1))
         self.loss = cos_similarity(self.weight.view(self.groups, -1))
         self.loss.backward()
         self.opt_orth.step()
@@ -52,9 +47,7 @@ class OrthorTransform(nn.Module):
     def forward(self, feat):
         if self.training:
             self.orthogonal_update()
-#        self.loss = 0
         pred = feat * self.weight.expand_as(feat)
-#        print(f"[DEBUG:module:OrthorTransform] pred.shape: {pred.shape}")
         return pred.mean(-1).mean(-1), self.loss
 
 
@@ -75,21 +68,7 @@ class CodeReduction(nn.Module):
         feat,loss_conv = self.main(feat)
         pred_c,loss_trans = self.trans(feat)
         return pred_c.view(feat.size(0), -1), loss_conv, loss_trans
-#        return feat.view(feat.size(0), -1), self.loss_conv_sum
 
-#class CodeReduction(nn.Module):
-#    def __init__(self, c_dim, feat_hw, blocks = 5, prob=False):
-#        super(CodeReduction, self).__init__()
-#        if prob:
-#            c_dim *= 2
-#        
-#        self.trans = OrthorTransform(c_dim=c_dim, feat_hw=feat_hw, groups = blocks)
-#        self.loss_trans_sum = 0.
-#    
-#    def forward(self, feat):
-#        pred_c,loss_trans = self.trans(feat)
-#        self.loss_trans_sum += loss_trans
-#        return pred_c.view(feat.size(0), -1), self.loss_trans_sum
 
 def cos_similarity(weight):
     weight = weight / weight.norm(dim=-1).unsqueeze(-1)
